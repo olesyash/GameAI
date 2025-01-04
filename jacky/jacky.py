97c1bfdc891958aae89b552bddc19d50daf9ac16
olesya <ilana.olesya@gmail.com>
2025-01-04 17:19:33 +0200

    Add random option to the game

--- jacky/jacky.py
+++ jacky/jacky.py
@@ -1,12 +1,18 @@
 from random import randint
 from consts import *
-
+import numpy as np
 
 class Game:
-    def __init__(self):
+    def __init__(self, random=False):
         self.sum = 0
         self.reward = 0
         self.status = ONGOING
+        self.random = random
+        self.my_moves = []
+        if random:
+            # Initialize reward array with random values from 0 to 21
+            self.rewards = np.random.randint(0, 22, size=22)
+            print("Rewards:", self.rewards)
 
     def make_move(self, move: str):
         if move == "hit":
@@ -14,25 +20,38 @@ class Game:
             new_card = randint(1, 10)
             self.sum += new_card
             print(f"New card: {new_card}")
-            print("Your sum:", self.sum)
-            self.check_reward()
+            if self.random:
+                print(f"Reward: {self.rewards[new_card]}")
+                print("Your sum:", self.sum)
+            else:
+                print("Your sum:", self.sum)
+            self.check_reward(new_card)
+            self.my_moves.append(new_card)
         elif move == "stay":
-            self.check_reward()
+            self.check_reward(self.my_moves[-1])
             self.status = 0
         else:
             print("Invalid move")
 
-    def check_reward(self):
+    def check_reward(self, new_card):
         if self.sum > JACKPOT:
             self.reward = 0
             self.status = 0
         else:
-            self.reward = self.sum
+            if self.random:
+                self.reward = self.rewards[new_card]
+            else:
+                self.reward = self.sum
         return self.reward
 
 
 def main():
-    game = Game()
+    # get from user if he wants to play with random rewards
+    random = input("Do you want to play with random rewards? (Y/N): ")
+    if random.lower() == "y":
+        game = Game(random=True)
+    else:
+        game = Game()
     # get input from user
     while game.status == ONGOING:
         move = input("Enter move: ")
