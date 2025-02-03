70d26041d50463c439ae7369ac69814f4f5fbf6d
mosheshy <mosheaa@gmail.com>
2025-02-03 15:06:48 +0200

    m4

--- Gomoku/gui.py
+++ Gomoku/gui.py
@@ -172,12 +172,12 @@ class GomokuGUI:
         # Perform AI move
         # MCTS
         if self.game_mode == MCTS:
-            best_node = self.mcts.search(self.game.clone(), iterations=7000)
+            best_node = self.mcts.search(self.game.clone(), iterations=10000)
             if self.game.make_move(best_node.state.last_move):
                 self.draw_board()
         # PUCT
         elif self.game_mode == PUCT:
-            best_move = self.puct_player.best_move(self.game.clone(), iterations=1000)
+            best_move = self.puct_player.best_move(self.game.clone(), iterations=10000)
             if self.game.make_move(best_move):
                 self.draw_board()
 
